f9a5b9fa46b7d695feb49ca95206e024b30ab5ab
Vira <qa.virap@gmail.com>
2025-06-11 23:09:11 +0200

    Add Comment class methods

--- modules/comment.py
+++ modules/comment.py
@@ -9,3 +9,11 @@ class Comment:
     def get_list_comments(self):
         path = f"/list/{list_id}/comment"
         return self.sender.get(path)
+
+    def create_list_comment(self, payload):
+        path = f"/list/{list_id}/comment"
+        return self.sender.post(path, payload)
+
+    def delete_comment(self, comment_id):
+        path = f"/comment/{comment_id}"
+        return self.sender.delete(path)
