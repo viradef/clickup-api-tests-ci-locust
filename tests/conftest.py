254156ebf9ab989512ff1314722583b523b64e8c
Vira <qa.virap@gmail.com>
2025-06-11 23:07:50 +0200

    Add create list comment fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from config.settings import *
+from modules.comment import Comment
 from modules.sender import Sender
+from faker import Faker
+fake = Faker()
 
 @pytest.fixture
 def authorized_sender():
@@ -9,3 +12,23 @@ def authorized_sender():
 @pytest.fixture
 def unauthorized_sender():
     return Sender(base_url=base_url)
+
+@pytest.fixture
+def comment_payload():
+    return {
+        "comment_text": fake.sentence(),
+        "assignee": None,
+        "notify_all": True
+    }
+
+@pytest.fixture
+def new_list_comment(comment_payload, authorized_sender):
+    comment = Comment(authorized_sender)
+
+    response = comment.create_list_comment(comment_payload)
+    response.raise_for_status()
+    comment_id = response.json()["id"]
+
+    yield comment, comment_payload, comment_id
+
+    comment.delete_comment(comment_id)
