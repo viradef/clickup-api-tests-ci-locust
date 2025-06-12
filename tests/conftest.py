9cf9d8c9763c32f258a3e1f91cd02c72d967e150
Vira <qa.virap@gmail.com>
2025-06-12 07:24:05 +0200

    Refactor generate functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,9 @@ import pytest
 from config.settings import *
 from modules.comment import Comment
 from modules.sender import Sender
+from tests.testdata import generate_comment_payload
 from faker import Faker
+
 fake = Faker()
 
 @pytest.fixture
@@ -13,18 +15,11 @@ def authorized_sender():
 def unauthorized_sender():
     return Sender(base_url=base_url)
 
-@pytest.fixture
-def comment_payload():
-    return {
-        "comment_text": fake.sentence(),
-        "assignee": None,
-        "notify_all": True
-    }
 
 @pytest.fixture(scope="function")
-def new_list_comment(comment_payload, authorized_sender):
+def new_list_comment(authorized_sender):
     comment = Comment(authorized_sender)
-
+    comment_payload = generate_comment_payload()
     response = comment.create_list_comment(comment_payload)
     response.raise_for_status()
     comment_id = response.json()["id"]
@@ -34,9 +29,9 @@ def new_list_comment(comment_payload, authorized_sender):
     comment.delete_comment(comment_id)
 
 @pytest.fixture(scope="function")
-def new_task_comment(comment_payload, authorized_sender):
+def new_task_comment(authorized_sender):
     comment = Comment(authorized_sender)
-
+    comment_payload = generate_comment_payload()
     response = comment.create_task_comment(comment_payload)
     response.raise_for_status()
     comment_id = response.json()["id"]
@@ -44,7 +39,3 @@ def new_task_comment(comment_payload, authorized_sender):
     yield comment, comment_payload, comment_id
 
     comment.delete_comment(comment_id)
-
-@pytest.fixture
-def generate_random_sentence():
-    return fake.sentence()
--- tests/testdata.py
+++ tests/testdata.py
@@ -1,11 +1,21 @@
 from faker import Faker
 
-fake = Faker()
+fake_en = Faker()
 fake_ua = Faker("uk_UA")
 
 update_comment_text_cases = [
-    (fake.sentence(nb_words=6), 200),
+    (fake_en.sentence(nb_words=6), 200),
     (fake_ua.sentence(nb_words=6), 200),
-    (fake.text(max_nb_chars=255), 200),
-    (f"{fake.word()} {fake.random_number(digits=5)}", 200),
+    (fake_en.text(max_nb_chars=255), 200),
+    (f"{fake_en.word()} {fake_en.random_number(digits=5)}", 200),
 ]
+
+def generate_random_sentence():
+    return fake_en.sentence()
+
+def generate_comment_payload(comment_text=None, assignee=None, notify_all=True):
+    return {
+        "comment_text": comment_text or fake_en.sentence(),
+        "assignee": assignee,
+        "notify_all": notify_all
+    }
