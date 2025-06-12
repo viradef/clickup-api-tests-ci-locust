b9577d8912e4d28521dc88e28053e2729c025274
Vira <qa.virap@gmail.com>
2025-06-12 07:08:19 +0200

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -6,6 +6,7 @@ load_dotenv()
 base_url = os.getenv("BASE_URL")
 api_key = os.getenv("API_KEY")
 list_id = os.getenv("LIST_ID")
+task_id = os.getenv("TASK_ID")
 
-if not all([base_url, api_key, list_id]):
+if not all([base_url, api_key, list_id, task_id]):
     raise ValueError("One or more required environment variables are not set.")
