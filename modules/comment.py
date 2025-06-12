ca119b1211cfdad22a05e3e5db7fbbed7f7192d3
Vira <qa.virap@gmail.com>
2025-06-12 06:57:15 +0200

    Add Comment class methods

--- modules/comment.py
+++ modules/comment.py
@@ -2,22 +2,39 @@ from config.settings import *
 from modules.sender import Sender
 
 class Comment:
-    def __init__(self, sender: Sender, custom_list_id: str | None = None):
+    def __init__(self, sender: Sender, custom_list_id: str | None = None, custom_task_id: str | None = None):
         self.sender = sender
         self.list_id = custom_list_id or list_id
-
-    def get_list_comments(self):
-        path = f"/list/{list_id}/comment"
-        return self.sender.get(path)
+        self.task_id = custom_task_id or task_id
 
     def create_list_comment(self, payload):
         path = f"/list/{list_id}/comment"
         return self.sender.post(path, payload)
 
-    def delete_comment(self, comment_id):
-        path = f"/comment/{comment_id}"
-        return self.sender.delete(path)
+    def get_list_comments(self):
+        path = f"/list/{list_id}/comment"
+        return self.sender.get(path)
 
     def update_comment(self, payload, comment_id):
         path = f"/comment/{comment_id}"
         return self.sender.put(path, payload)
+
+    def delete_comment(self, comment_id):
+        path = f"/comment/{comment_id}"
+        return self.sender.delete(path)
+
+    def create_threaded_comment(self, payload, comment_id):
+        path = f"/comment/{comment_id}/reply"
+        return self.sender.post(path, payload)
+
+    def get_threaded_comments(self, comment_id):
+        path = f"/comment/{comment_id}/reply"
+        return self.sender.get(path)
+
+    def create_task_comment(self, payload):
+        path = f"/task/{task_id}/comment"
+        return self.sender.post(path, payload)
+
+    def get_task_comments(self):
+        path = f"/task/{task_id}/comment"
+        return self.sender.get(path)
